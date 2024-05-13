b87ad86bdf57ee589c6c0f704ada531cc0249ada
Denys <omelchenko4562@gmail.com>
2024-05-13 23:32:48 +0300

    modified component Home

--- shop/front/shop_user/src/Components/Home/Home.jsx
+++ shop/front/shop_user/src/Components/Home/Home.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import Logo from '../img/Logo.png'
 import './Home.css'
+
 const Home = () => {
   const [userData, setUserData] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const [modalClose, SetShowModal] = useState(false);
+  const [buttonVisible, setButtonVisible] = useState(true); 
   useEffect(() => {
     const authToken = localStorage.getItem('authToken');
-
     
     if (authToken) {
       fetch('http://localhost:5000/api', {
         headers: {
           Authorization: `Bearer ${authToken}`
         }
-       
       })
       .then(response => {
-        
         return response.json();
       })
       .then(data => {
@@ -28,44 +30,72 @@ const Home = () => {
   }, []);
 
 
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const getBackgroundColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16); // Генерація випадкового кольору
+    return `#${randomColor}`;
   };
 
+  const getInitials = () => {
+    if (userData && userData.username) {
+      const username = userData.username.trim();
+      return username.charAt(0).toUpperCase(); // Перша буква логіна
+    }
+    return '';
+  };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    SetShowModal(true);
+    setButtonVisible(false); 
+  };
 
+  const close =  () =>{
+    setIsOpen(false) 
+    setButtonVisible(true)
+  }
 
   return (
-  
+    <div>
+      {buttonVisible && ( 
+        <div>
+          <button className="burger-menu burger-menu-toggle" onClick={toggleMenu}>
+            <span className="burger-menu-icon"></span>
+          </button>
+        </div>
+      )}
 
-    <div className="burger-menu">
-    {/* Кнопка бургер-меню */}
-    <button className="burger-menu-toggle" onClick={toggleMenu}>
-      <span className="burger-menu-icon"></span>
-    </button>
-
-    {/* Меню */}
-    <div className={`burger-menu-content ${isOpen ? 'open' : ''}`}>
-      <ul>
-      {userData ? (
+      <div className={`burger-menu-content ${isOpen ? 'open' : ''}`}>
+          {modalClose &&
+            <span className="close" onClick={close}>
+              &times;
+            </span>
+          }
+         
+          {userData ? (
             <div>
-              
-              <p>Ім'я: {userData.username}</p>
-              <p>Email: {userData.email}</p>
-              <p>Login: {userData.login}</p>
-              {/* <img src={} alt="Random image" /> */}
-              
-              {/* Інші дані користувача */}
+    
+              <img src={Logo} alt='logo' className='logo' />
+              <div className="main-block">
+                <div className='backgroundItems'>
+                  <div className="content-width">
+                    <div className="user-icon"  style={{backgroundColor: getBackgroundColor()}}>
+                    {getInitials()}
+                    </div>
+                  </div>
+                  <div className="data-user">
+                    <div className='data-style-login'>{userData.username} {userData.login}</div>
+                    <div>{userData.email}</div>
+                  </div>
+                </div>              
+              </div>
             </div>
           ) : (
             <p>Завантаження...</p>
           )}
-        {/* Додайте інші пункти меню, які вам потрібно */}
-      </ul>
+        
+      </div>
     </div>
-    </div>
-      );
-    };
+  );
+};
 
 export default Home;
